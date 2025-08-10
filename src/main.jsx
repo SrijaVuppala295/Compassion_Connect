c4e857efa0ea5eb7ec7682b1866a7f370e5ab684
Mavish Sethi <mavishsethi@gmail.com>
2025-08-10 22:18:41 +0530

    added leaderboard page

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,23 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home.jsx';
+import Leaderboard from './pages/Leaderboard.jsx';
 import Contact from './pages/contact.jsx';
 import Event from './pages//event.jsx';
 import Feedback from './pages/feedback.jsx';
 import VolunteerRegistration from './pages/volunteer.jsx';
-import Navbar from './pages/Navbar.jsx';        
+import Navbar from './pages/Navbar.jsx';   
+import HashRedirect from './pages/HashRedirect.jsx';     
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Navbar />
+      <HashRedirect />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/event/:id" element={<Event />} />
         <Route path="/feedback" element={<Feedback />} />
         <Route path="/volunteer" element={<VolunteerRegistration />} />
